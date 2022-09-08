dea8f9c25bbbf575a7ffd9573ee8420962823305
Fatih <justicestranger@gmail.com>
2022-09-08 03:53:34 +0300

    fix: flatted is not permitted in heroku?

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-var { parse, stringify, toJSON, fromJSON } = require('flatted');
 const dns = require('node:dns');
-const { nextTick } = require('node:process');
-const { link } = require('node:fs');
 
 const arrayOfURLs = [];
 // Basic Configuration
