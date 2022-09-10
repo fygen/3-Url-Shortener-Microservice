6a6115966dc31e36c6041cf7b2280386edb6fd9d
Fatih <justicestranger@gmail.com>
2022-09-10 08:15:04 +0300

    Finished late, due to a conversion of new URL method.

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const bodyParser = require('body-parser');
 const app = express();
 const dns = require('node:dns');
 
-const arrayOfURLs = [];
+const arrayOfURLs = ["Wrong Format"];
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
 app.use((req, res, next) => {
   console.log(req.method, req.path, req.ip)
   next();
 })
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 // Middleware for parsing req.body 
@@ -30,27 +32,26 @@ app.get('/api/hello', function (req, res) {
 
 app.post('/api/shorturl', (req, res) => {
   let checkLink = req.body.url;
-  let regHttps = /(https?:\/\/)/
-  let regEndSlash = /\/+$/
-
-  if (checkLink.includes("https://") || checkLink.includes("http://")) {
-    let lookupLink = checkLink.replace(regHttps, "").replace(regEndSlash,"");
-    console.log(lookupLink);
-    dns.lookup(lookupLink, (err) => {
-      if (err) res.json({ "error": "Invalid URL" })
-      else {
-        arrayOfURLs.indexOf(checkLink) == -1 ? arrayOfURLs.push(checkLink) : null;
-        res.json({ "original_url": checkLink.replace(regEndSlash,""), "short_url": arrayOfURLs.indexOf(checkLink) })
-      }
-    })
-  }
-  else {
-    res.json({ "error": "Invalid URL" })
-  }
+  const url = new URL(req.body.url).hostname;
+  dns.lookup(url, (err) => {
+    if (err) res.json({ "error": "Invalid URL" })
+    else {
+      arrayOfURLs.indexOf(checkLink) == -1 ? arrayOfURLs.push(checkLink) : null;
+      res.json({ "original_url": req.body.url, "short_url": arrayOfURLs.indexOf(checkLink) })
+    }
+  })
 })
 
 app.get('/api/shorturl/:index', (req, res) => {
-  res.status(301).redirect(arrayOfURLs[req.params.index]);
+  if (req.params.index == 0) {
+    res.json({ "error": "Wrong format" });
+  }
+  else if (req.params.index == undefined) {
+    res.json({ "error": "Invalid number" })
+  }
+  else {
+    res.status(301).redirect(arrayOfURLs[req.params.index]);
+  }
 })
 
 app.listen(port, function () {
